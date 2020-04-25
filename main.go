7c1aa248dd679fd2a48047efbe7f25f685b4604e
Rahul Dani <32138368+15dani1@users.noreply.github.com>
2020-04-25 12:11:18 -0400

    Almost done with MongoDB

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"time"
 
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -14,8 +16,13 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	ctx, _ := content.WithTimeout(context.Background(), 10*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	err = client.Ping(ctx, readpref.Primary())
 	if err != nil {
 		log.Fatal(err)
 	}
