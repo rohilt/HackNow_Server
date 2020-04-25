75e7bdba5c916d116d580316779a81f01beb7b8c
Rahul Dani <32138368+15dani1@users.noreply.github.com>
2020-04-25 12:02:18 -0400

    Setting up MongoDB

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func testing(w http.ResponseWriter, req *http.Request) {
-	w.Write([]byte("You sent a request to /hello!"))
-}
-
 func main() {
-	fmt.Printf("Server started")
-	http.HandleFunc("/hello", testing)
-	http.ListenAndServe(":5000", nil)
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://15dani1:hacknow@cluster0-f47on.gcp.mongodb.net/test?retryWrites=true&w=majority"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := content.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
